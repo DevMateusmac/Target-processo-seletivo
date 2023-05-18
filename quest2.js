bca6ac8ffbe40f1dee9826372f781147cf70669b
Mateus Maciulevicius <dev.mateusmac@gmail.com>
2023-05-18 14:54:01 -0300

    corrigindo exercício

--- quest2.js
+++ quest2.js
@@ -1,24 +1,23 @@
 
-function belongToFibonacci(number){
-  let fibonacciSequence = []
-  fibonacciSequence[0] = 0
-  fibonacciSequence[1] = 1
+let fibonacciSequence = []
+let initialNumber = 0 
+let nextNumber = 1
+let lastNumber 
 
-  
-  for(let i = 2; i < number; i++){
-    fibonacciSequence[i] = fibonacciSequence [i - 2] + fibonacciSequence [i - 1]
-    if(fibonacciSequence[i] >= number){
-      break
-    }
+function belongToFibonacciSequence(number){
+  fibonacciSequence.push(initialNumber, nextNumber)
+
+  while(!fibonacciSequence.includes(number) && fibonacciSequence[fibonacciSequence.length - 1] < number){
+    lastNumber = initialNumber + nextNumber
+    fibonacciSequence.push(lastNumber)
+    initialNumber = nextNumber
+    nextNumber = lastNumber
   }
-
   if(fibonacciSequence.includes(number)){
-    console.log(fibonacciSequence)
-    return console.log(`O número ${number} pertence à sequência de Fibonacci`)
+    console.log(`O número ${number} pertence à sequencia Fibonacci`)
   }else{
-    console.log(fibonacciSequence)
-    return console.log(`O número ${number} não pertence à sequência de Fibonacci`)
+    console.log(`O número ${number} não pertence à sequencia Fibonacci`)
   }
 }
 
-belongToFibonacci(89) // neste caso 89 pertence à sequencia fibonacci
+belongToFibonacciSequence(89) // neste caso 89 pertence à sequencia fibonacci
